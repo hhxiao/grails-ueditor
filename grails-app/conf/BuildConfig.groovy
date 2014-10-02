44cdc75bba9ebd6fdc2d465bd292abfd0911e048
Hai-Hua Xiao <hhxiao@gmail.com>
2014-10-02 09:35:02 +0800

    update build plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,9 +29,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
-            export = false
-        }
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			export = false
+		}
     }
 }
